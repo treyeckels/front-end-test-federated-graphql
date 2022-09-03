fee2bab845febb3e9e0689288417a8c883658cac
Trey Eckels <trey.eckels@gmail.com>
2022-09-03 10:09:20 -0400

    Updates typedefs.

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -37,7 +37,12 @@ const typeDefs = gql`
 
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
     saveBook(bookData: BookInput!): User
     removeBook(bookId: ID!): User
   }
